d7cdf59899c6fec82b224663fb50cbb9a31c29a5
honoka <xal821792703@163.com>
2016-02-23 23:31:25 +0800

    初始化task0002

---
+++ 2015_spring/Intermediate/task0002/src/js/util.js
@@ -0,0 +1,21 @@
+/**
+ * Created by honoka on 16/2/23.
+ */
+var util = function() {
+    /**
+     * 是否是数组类型
+     * @param {Array} arr 判断数组
+     * @retrun Boolean true 是数组类型
+     */
+    function isArr(arr) {
+
+    }
+    /**
+     * 是否是函数类型
+     * @param {Function} fn 被判断函数
+     * @retrun Boolean true 是函数类型
+     */
+    function isFunction(fn) {
+
+    }
+}();
