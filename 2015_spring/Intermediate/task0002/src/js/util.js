c5072bf08813393a478e0a68317c527ca4a37609
honoka <xal821792703@163.com>
2016-02-24 23:22:43 +0800

    判断是否是数组方法

--- 2015_spring/Intermediate/task0002/src/js/util.js
+++ 2015_spring/Intermediate/task0002/src/js/util.js
@@ -1,5 +1,6 @@
 /**
  * Created by honoka on 16/2/23.
+ * 判断数据类型工具库
  */
 var util = function() {
     /**
@@ -8,7 +9,9 @@ var util = function() {
      * @retrun Boolean true 是数组类型
      */
     function isArr(arr) {
-
+        //当页面存在多个全局作用域时，使用 instanceof 判断不同作用域的引用类型会造成混乱
+        //故调用 Object 原生方法 toString 判断
+        return Object.prototype.toString.call(arr) == "[object Array]";
     }
     /**
      * 是否是函数类型
